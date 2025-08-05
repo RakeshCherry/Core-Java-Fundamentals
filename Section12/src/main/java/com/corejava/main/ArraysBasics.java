f1e08a1e19978ffde8995eaa795771b1290d560f
RAKESH M B <rakeshbhavimani1@gmail.com>
2025-08-05 13:04:33 +0530

    Arrays

---
+++ Section12/src/main/java/com/corejava/main/ArraysBasics.java
@@ -0,0 +1,27 @@
+package com.corejava.main;
+
+import com.corejava.main.model.Person;
+
+public class ArraysBasics {
+public static void main(String[] args) {
+
+        int [] mobileNumbers = new int[5];
+        mobileNumbers[0] = 789702603;
+        mobileNumbers[1] = 769707003;
+        mobileNumbers[2] = 563948230;
+        mobileNumbers[3] = 327593485;
+        mobileNumbers[4] = 872580932;
+
+
+
+        int numbers[] = {1,2,3,4,5};
+        double[] prices;
+        char[]grades;
+        String [] names;
+
+        int length = 100;
+        Person[] persons = new Person[length*2];
+    }
+}
+
+
---
+++ Section12/src/main/java/com/corejava/main/model/Person.java
@@ -0,0 +1,4 @@
+package com.corejava.main.model;
+
+public class Person {
+}
